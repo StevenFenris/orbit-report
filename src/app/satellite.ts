01152a5366b6404f1cb92a5cc40eb8a50ce59914
Steve Larson <slarson1@pnmac.com>
2019-10-30 17:25:28 -0500

    Satellite array constructor

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,2 +1,17 @@
 export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+
+    constructor(currentname: string, currenttype: string, currentlaunchDate: string, currentorbitType: string, currentoperational: boolean){
+        this.name = currentname;
+        this.type = currenttype;
+        this.launchDate = currentlaunchDate;
+        this.orbitType = currentorbitType;
+        this.operational = currentoperational;
+    }
 }
+
+
